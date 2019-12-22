04d811e21f6851be76b2d81456f9e1958dfa50b8
Itai Tenenbaum <taitulism@gmail.com>
2019-12-22 23:19:52 +0200

    postcss

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const cssNano = require('cssnano');
+const cssImport = require('postcss-import');
+
+module.exports = {
+    plugins: [
+        cssImport({
+            from: './src/reset.css',
+        }),
+        cssNano({
+            preset: 'default',
+        }),
+    ],
+};
