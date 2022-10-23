83e8ab5abc0e18498e5e905c86871987515681a8
Itai Tenenbaum <taitulism@gmail.com>
2022-10-24 00:07:35 +0300

    .editorconfig (tab=3)

--- playground/playground.js
+++ playground/playground.js
@@ -3,63 +3,63 @@ const {$, createElm, bindEvent} = sweetDom;
 const showElm = (elm) => elm.classList.remove('invisible');
 const hideElm = (elm) => elm.classList.add('invisible');
 const capitalizeFirstLetter = (str) => {
-    const upperFirst = str[0].toUpperCase();
-    return upperFirst + str.substr(1);
+	const upperFirst = str[0].toUpperCase();
+	return upperFirst + str.substr(1);
 };
 
 let navBar;
 
 const topicElems = {};
 const topicList = [
-    'form',
-    'list',
-    'article',
-    'flex',
-    'others',
+	'form',
+	'list',
+	'article',
+	'flex',
+	'others',
 ];
 
 function init () {
-    navBar = $('#topic-navigation');
+	navBar = $('#topic-navigation');
 
-    topicList.forEach((topic, i) => {
-        // Elements
-        const btnText = capitalizeFirstLetter(topic);
-        const navBtn = createElm('button.nav-btn', btnText);
-        const topicElm = $(`#${topic}`);
-        
-        // Keep References
-        topicElems[topic] = [navBtn, topicElm];
+	topicList.forEach((topic, i) => {
+		// Elements
+		const btnText = capitalizeFirstLetter(topic);
+		const navBtn = createElm('button.nav-btn', btnText);
+		const topicElm = $(`#${topic}`);
 
-        // Initial Visibility
-        if (i === 0) {
-            showElm(topicElm);
-            navBtn.classList.add('selected');
-        }
-        else {
-            hideElm(topicElm);
-            navBtn.classList.remove('selected');
-        }
-        
-        // Bind Click
-        bindEvent(navBtn, 'click', () => switchTo(topic));
+		// Keep References
+		topicElems[topic] = [navBtn, topicElm];
 
-        // Append to DOM
-        navBar.append(navBtn);
-    }, topicElems);
+		// Initial Visibility
+		if (i === 0) {
+			showElm(topicElm);
+			navBtn.classList.add('selected');
+		}
+		else {
+			hideElm(topicElm);
+			navBtn.classList.remove('selected');
+		}
+
+		// Bind Click
+		bindEvent(navBtn, 'click', () => switchTo(topic));
+
+		// Append to DOM
+		navBar.append(navBtn);
+	}, topicElems);
 }
 
 function switchTo (wantedTopic) {
-    topicList.forEach((topic) => {
-        const topicNavBtn = topicElems[topic][0];
-        const topicContainer = topicElems[topic][1];
+	topicList.forEach((topic) => {
+		const topicNavBtn = topicElems[topic][0];
+		const topicContainer = topicElems[topic][1];
 
-        if (topic === wantedTopic) {
-            showElm(topicContainer);
-            topicNavBtn.classList.add('selected');
-        }
-        else {
-            hideElm(topicContainer);
-            topicNavBtn.classList.remove('selected');
-        }
-    });
+		if (topic === wantedTopic) {
+			showElm(topicContainer);
+			topicNavBtn.classList.add('selected');
+		}
+		else {
+			hideElm(topicContainer);
+			topicNavBtn.classList.remove('selected');
+		}
+	});
 }
--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,12 @@ const cssNano = require('cssnano');
 const cssImport = require('postcss-import');
 
 module.exports = {
-    plugins: [
-        cssImport({
-            from: './src/reset.css',
-        }),
-        cssNano({
-            preset: 'default',
-        }),
-    ],
+	plugins: [
+		cssImport({
+			from: './src/reset.css',
+		}),
+		cssNano({
+			preset: 'default',
+		}),
+	],
 };
